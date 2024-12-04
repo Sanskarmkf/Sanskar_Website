e19f3c92a52da861ec67fc32c3c36059649fe491
Sanskar <sanskarmkf@gmail.com>
2024-12-04 23:17:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -168,4 +168,4 @@ def program3():
     return render_template('program3.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
